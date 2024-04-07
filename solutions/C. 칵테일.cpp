98a98bd022ee1b7a397f05ff8419ae2696c0869e
Gyeongmin Lee <lee_gyeongmin@naver.com>
2024-04-07 21:38:17 +0900

    Update problem C

---
+++ solutions/C. 칵테일.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int K, N;
+    cin >> K >> N;
+    vector<int> v(N);
+    for (int &a : v) {
+        cin >> a;
+    }
+
+    for (int i = 0; i < (1 << N); i++) {
+        int sum = 0;
+        int cnt = 0;
+        for (int j = 0; j < N; j++) {
+            if (i & (1 << j)) {
+                sum += v[j];
+                cnt++;
+            }
+        }
+
+        int avg = floor(sum / cnt) + 0.5;
+        if (avg == K) {
+            cout << "YES";
+            return 0;
+        }
+    }
+
+    cout << "NO";
+    return 0;
+}
+
+/////////////////////////////////////////
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int K, N;
+vector<int> v;
+bool answer = false;
+
+void recursive(int depth, int sum, int cnt) {
+    if (depth == N) {
+        int avg = floor(sum / cnt) + 0.5;
+        if (avg == K) {
+            answer = true;
+        }
+        return;
+    }
+
+    recursive(depth + 1, sum, cnt);
+    recursive(depth + 1, sum + v[depth], cnt + 1);
+}
+
+int main() {
+    cin >> K >> N;
+
+    v.resize(N);
+    for (int &a : v) {
+        cin >> a;
+    }
+
+    recursive(0, 0, 0);
+
+    cout << (answer ? "YES" : "NO");
+
+    return 0;
+}
