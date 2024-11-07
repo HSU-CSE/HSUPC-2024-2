2d6a6d3de01a063fb1b0df7888895a1341277abd
Yunbin Chang <yunbin@hansung.ac.kr>
2024-11-07 16:56:06 +0900

    Create B. 부기의 MT 준비.cpp

---
+++ solutions/B. 부기의 MT 준비.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+
+void solve() {
+    int n = 1;
+    int m = 1;
+
+    while(n * m < N) {
+        if (n > m) m++;
+        else n++;
+    }
+
+    cout << n * m;
+}
+
+void input() {
+    cin >> N;
+}
+
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    input();
+    solve();
+    return 0;
+}
