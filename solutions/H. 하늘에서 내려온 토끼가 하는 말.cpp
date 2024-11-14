f959841c8a0e940b4bea2cdbb6e5fcb1622cf35f
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-11-14 18:20:18 +0900

    Update H. 하늘에서 내려온 토끼가 하는 말.cpp

--- solutions/H. 하늘에서 내려온 토끼가 하는 말.cpp
+++ solutions/H. 하늘에서 내려온 토끼가 하는 말.cpp
@@ -4,58 +4,72 @@ using namespace std;
     1. 물 먼저 bfs
     2. 토끼 bfs
 */
+const int INF = 0x3f3f3f3f;//끝 상수
+const int SIZE = 102;
+vector<string> board;//보드
 
-const int INF = 0x3f3f3f3f;
-vector<string> board; // 보드
-int n, m;
-
-int dist(pair<int, int> &e_point, pair<int, int> tmp)
-{
-    return abs(e_point.first - tmp.first) + abs(e_point.second - tmp.second);
-}
-int get_dist(pair<int, int> &e_point, char target)
-{
-    int ret{INF};
-    for (int i{}; i < n; i++)
-    {
-        for (int j{}; j < m; j++)
-        {
-            if (board[i][j] == target)
-            {
-                ret = min(ret, dist(e_point, {i, j}));
-            }
-        }
-    }
-
-    return ret;
-}
-int main()
-{
-    cin.tie(0)->sync_with_stdio(0);
-
-    cin >> n >> m;
+int dx[]{0,0,1,-1};
+int dy[]{1,-1,0,0};
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    int n,m;cin >> n >> m;
     board.resize(n);
+    
+    for(auto &row: board)cin >> row;
 
-    for (auto &row : board)
-        cin >> row;
+    queue<pair<int,int>> b_q, w_q;//토끼, 물;
 
-    pair<int, int> e_point;
+    int b_visited[SIZE][SIZE], c_visited[SIZE][SIZE];
+    memset(b_visited, 0x3f, sizeof b_visited);
+    memset(c_visited, 0x3f, sizeof c_visited);
 
-    for (int i{}; i < n; i++)
-    {
-        for (int j{}; j < m; j++)
-        {
-            if (board[i][j] == 'C')
-            {
-                e_point = {i, j};
+    int e_x,e_y;
+    for(int i{}; i < n; i++){
+        for(int j{}; j < m; j++){
+            if(board[i][j] == 'B'){//토끼
+                b_visited[i][j] = 0;
+                b_q.push({i,j});
+            }
+            else if(board[i][j] == '*'){//물
+                c_visited[i][j] = 0;
+                w_q.push({i,j});
+            }
+            else if(board[i][j] == 'C'){
+                e_x = i,e_y = j;
             }
         }
     }
 
-    int water_dist = get_dist(e_point, '*');
-    int bunny_dist = get_dist(e_point, 'B');
-    if(bunny_dist < water_dist)cout << bunny_dist;
-    else cout << "Umchichi..Umchichi..";
+    while(!w_q.empty()){//물 먼저 bfs
+        auto [x,y] = w_q.front();w_q.pop();
+        for(int i{};i<4;i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m)continue;
+            if(c_visited[nx][ny] != INF)continue;
+            c_visited[nx][ny] = c_visited[x][y] + 1;
+            w_q.push({nx,ny});
+        }
+    }
 
+    while(!b_q.empty()){
+        auto [x,y] = b_q.front(); b_q.pop();
+        for(int i{};i<4;i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m)continue;
+            
+            int mark = b_visited[x][y] +  1;
+            if(mark >= c_visited[nx][ny] || b_visited[nx][ny] != INF)continue;
+            //내가 가려는 곳에 물이 이미 도착했음
+            b_visited[nx][ny] = mark;
+            b_q.push({nx,ny});
+        }
+    }
+    if(b_visited[e_x][e_y] == INF){
+        cout << "Umchichi..Umchichi..";
+    }else{
+        cout << b_visited[e_x][e_y];
+    }
     return 0;
 }
